a8f1145321a94d0ea9917d6249b35fb16010d361
Philippe Groarke <philippe.groarke@gmail.com>
2017-03-31 00:43:57 -0400

    use join

--- image_fill/image_fill.py
+++ image_fill/image_fill.py
@@ -8,7 +8,9 @@ if len(sys.argv) != 3:
 	print "image_fill\nUsage - ./image_fill.py [directory] [image]\n"
 	quit()
 
+copyfile(sys.argv[2], os.path.join(sys.argv[1], sys.argv[2]))
+
 for root, dirs, files in os.walk(str(sys.argv[1])):
 	for dir in dirs:
 		print os.path.join(root, dir, sys.argv[2])
-		copyfile(sys.argv[2], os.path.join(root, dir) + "/" + sys.argv[2])
+		copyfile(sys.argv[2], os.path.join(root, dir, sys.argv[2]))
