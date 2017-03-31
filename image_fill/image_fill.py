ce57b8b3326afad22b54a12822070b0b4a3f8022
Philippe Groarke <philippe.groarke@illogika.com>
2017-03-31 13:03:11 -0400

    Catch IOError

--- image_fill/image_fill.py
+++ image_fill/image_fill.py
@@ -13,4 +13,7 @@ copyfile(sys.argv[2], os.path.join(sys.argv[1], sys.argv[2]))
 for root, dirs, files in os.walk(str(sys.argv[1])):
 	for dir in dirs:
 		print os.path.join(root, dir, sys.argv[2])
-		copyfile(sys.argv[2], os.path.join(root, dir, sys.argv[2]))
+		try:
+			copyfile(sys.argv[2], os.path.join(root, dir, sys.argv[2]))
+		except IOError as e:
+			"Skipping " + os.path.join(root, dir) + "\n" + e.strerror
